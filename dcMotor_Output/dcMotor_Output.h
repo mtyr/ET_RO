d0d555efe340f118596e6f706b4436a61f242770
Xingyue3 <40678169+Xingyue3@users.noreply.github.com>
2018-07-17 15:13:53 +0900

    Add files via upload

---
+++ dcMotor_Output/dcMotor_Output.h
@@ -0,0 +1,17 @@
+
+#include "Motor.h"
+#include "ev3api.h"
+#include "app.h"
+
+
+class dcMotor_Output{
+	
+	public:
+	void MotorOutput(int8_t getPwmLeft,int8_t getPwmRight);
+	
+	private:
+	Balancer* mBalancer;
+	int8_t mRightPwm;
+    int8_t mLeftPwm;
+
+};
